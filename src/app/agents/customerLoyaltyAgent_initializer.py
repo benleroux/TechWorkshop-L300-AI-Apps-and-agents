822ea6dc35357d969997ca9ab7f5c02d2226284d
benleroux <belero@microsoft.com>
2025-11-21 19:28:10 +0000

    Finished exercise 3

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -8,6 +8,7 @@ from typing import Callable, Set, Any
 from tools.discountLogic import calculate_discount
 # from tools.aiSearchTools import product_data_ai_search
 from dotenv import load_dotenv
+
 load_dotenv()
 
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
